8f5bb47a8056707715ceb4f1f0ad1c41b80e51c9
LUFFY DEV <denk2eror@gmail.com>
2025-03-11 13:20:38 +0700

    add line loggin version 4

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,6 +23,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="th">
+      <meta name="mobile-web-app-capable" content="yes" />
+      <meta name="apple-mobile-web-app-capable" content="yes" />
       <body style={{ margin: 0, padding: 0 }} className={noto_th.variable}>
         <AppRouterCacheProvider>
           <ThemeCustomProvider>
